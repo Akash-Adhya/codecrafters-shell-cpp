48664b7e13b77d126cc5b7146e7cdf6c89cc2d7a
Akash Adhya <akashadhya19@gmail.com>
2025-01-16 17:10:23 +0530

    REPL implementing

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
-#include<string>
+#include <string>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
 
-  std::string input;
-  std::getline(std::cin, input);
-  std::string err_msg = ": command not found\n";
+  while (true)
+  {
+    std::cout << "$ ";
 
-  std::cout<<input<<err_msg;
+    std::string input;
+    std::getline(std::cin, input);
+    std::string err_msg = ": command not found\n";
+
+    std::cout << input << err_msg;
+  }
 }
