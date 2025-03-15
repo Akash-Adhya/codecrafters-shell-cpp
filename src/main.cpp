9dd7c1423b767294069089de4c075caa9c217d06
Akash Adhya <akashadhya19@gmail.com>
2025-03-15 22:43:12 +0530

    Partial completion checking

--- src/main.cpp
+++ src/main.cpp
@@ -347,7 +347,7 @@ int main()
                     if (commonPrefix.length() > input.length())
                     {
                         cout << commonPrefix.substr(input.length()) << " ";
-                        input = commonPrefix + " ";
+                        input = commonPrefix;
                     }
                     else
                     {
